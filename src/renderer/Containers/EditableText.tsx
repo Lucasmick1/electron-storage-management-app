5d289e655cc668834490aa717258151298c88d4b
Lucasmick1 <lucas_29_costa@hotmail.com>
2023-07-28 16:21:05 -0300

    Creating EditableText component

--- src/renderer/Containers/EditableText.tsx
+++ src/renderer/Containers/EditableText.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import { RxCross1 } from 'react-icons/rx';
+import { LiaTimesCircle } from 'react-icons/lia';
 
 import { useClickAway } from 'renderer/modules/hooks';
 import Text from 'renderer/Components/Text';
@@ -22,15 +22,35 @@ const EditableText: React.FC<EditableTextProps> = ({ value, onChange }) => {
   });
 
   return !isEditing ? (
-    <Flex height="30px">
+    <Flex
+      height="30px"
+      width="336px"
+      style={{ padding: '8px 9px', boxSizing: 'border-box' }}
+      alignItems="center"
+    >
       <Text fontSize="22px" color="#484D59" onClick={() => setIsEditing(true)}>
         {value}
       </Text>
     </Flex>
   ) : (
     <Flex alignItems="center" gap="8px">
-      <Input value={newText} onChange={setNewText} ref={ref} />
-      <RxCross1 onClick={() => setIsEditing(false)} />
+      <Input
+        autoFocus={isEditing}
+        value={newText}
+        onChange={setNewText}
+        ref={ref}
+        fontSize="22px"
+        color="#484D59"
+        fontWeight="600"
+        backgroundColor="#eff0f2;"
+      />
+      <LiaTimesCircle
+        onClick={() => setIsEditing(false)}
+        fill="#FF7A00"
+        stroke="#FF7A00"
+        size="24px"
+        cursor="pointer"
+      />
     </Flex>
   );
 };
