cd88474667f35f25099e440c152f7d875382966b
Lucasmick1 <lucas_29_costa@hotmail.com>
2023-07-28 16:20:34 -0300

    Improving Input to handle ref

--- src/renderer/Components/Input.tsx
+++ src/renderer/Components/Input.tsx
@@ -1,30 +1,39 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef, CSSProperties, HTMLAttributes } from 'react';
 import { styled } from 'styled-components';
 
-type InputProps = {
-  value: string;
-  onChange: (value: string) => void;
-};
+type InputProps = CSSProperties &
+  Omit<HTMLAttributes<HTMLInputElement>, 'onChange'> & {
+    value: string;
+    onChange: (value: string) => void;
+  };
 const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ onChange, value }, ref) => (
-    <div>
-      <Wrapper
-        ref={ref}
-        type="text"
-        onChange={(e) => onChange(e.target.value)}
-        {...{ value }}
-      />
-    </div>
+  ({ onChange, value, ...rest }, ref) => (
+    <Wrapper
+      ref={ref}
+      type="text"
+      onChange={(e) => onChange(e.target.value)}
+      {...{ value, ...rest }}
+    />
   )
 );
 
-const Wrapper = styled.input`
+const Wrapper = styled.input<CSSProperties>`
   height: 30px;
   border-radius: 4px;
   border: 1px solid #6a7486;
   box-sizing: border-box;
   padding: 8px;
   outline: none;
+
+  ${(props) =>
+    Object.entries(props).reduce(
+      (acc, [key, value]) =>
+        `${acc}${key.replace(
+          /[A-Z]/g,
+          (m) => `-${m.toLowerCase()}`
+        )}:${value};`,
+      ''
+    )}
 `;
 
 export default Input;
